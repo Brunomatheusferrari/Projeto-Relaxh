18176b1b3afda9de5193973cdf56dc946e970289
Bruno Matheus Ferrari <brunomatheussferrari@gmail.com>
2021-09-29 17:18:02 -0300

    Inicio Tela Adm

---
+++ client/src/pages/AdminDelivery/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { AdminDContainer } from "../../components/AdminDContainer";
+
+
+export function AdminDelivery() {
+    return(
+      <AdminDContainer>
+          <h1 className="title">Gerenciamento de Serviços</h1>
+      </AdminDContainer>
+    );
+}
