6c6afb1f48f3b925c38a36346a5857f5bf2c2f31
Vinibilk <you@example.com>
2021-10-01 17:11:58 -0300

    Revisando Validations

--- HotelApp/services/api.js
+++ HotelApp/services/api.js
@@ -2,7 +2,7 @@ import Constants from "expo-constants";
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://392c-191-240-218-3.ngrok.io",
+    baseURL: "http://ed60-177-200-213-98.ngrok.io",
     timeout: 2000
 });
 
