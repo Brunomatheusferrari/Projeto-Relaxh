bbc357bbcaf929f89691c27dc43fb7a0efed41d8
Vinibilk <you@example.com>
2021-10-08 17:05:06 -0300

    Config Test

--- Server/src/config/database.js
+++ Server/src/config/database.js
@@ -1,11 +1,22 @@
 require("dotenv").config();
-const { Sequelize } = require("sequelize")
 
 module.exports = {
   development: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    logging: false,
+    define: {
+      underscored: true
+    }
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.TEST_DB_DATABASE,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT,
